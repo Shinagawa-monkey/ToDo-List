ea2e4d68df5cdf4b0094c47b7e0a3fbfc01b82ce
Elena Shatalova <59675339+Shinagawa-monkey@users.noreply.github.com>
2023-05-02 19:43:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,58 @@
-$(function() {
-  $("#add").on("click", function() {
-    var val = $("input").val();
-    if(val !== '') {
-     var elem = $("<li></li>").text(val);
-     $(elem).append("<button class='rem'>X</button>");
-     $("#mylist").append(elem);
-     $("input").val("");
-     $(".rem").on("click", function() {
-      $(this).parent().remove();
-     });
-    }
-  });
-});
+// $(function() {
+//   $("#add").on("click", function() {
+//     var val = $("input").val();
+//     if(val !== '') {
+//      var elem = $("<li></li>").text(val);
+//      $(elem).append("<button class='rem'>X</button>");
+//      $("#mylist").append(elem);
+//      $("input").val("");
+//      $(".rem").on("click", function() {
+//       $(this).parent().remove();
+//      });
+//     }
+//   });
+// });
 
 $("#item").click(function() {
   $("#submenu").slideToggle(500);
 }); 
+
+
+$(function() {
+  var $input = $("input");
+  var $mylist = $("#mylist");
+  var $submenu = $("#submenu");
+
+  if ($input.length === 0) {
+    console.error("Error: input field not found.");
+    return;
+  }
+
+  if ($mylist.length === 0) {
+    console.error("Error: list element not found.");
+    return;
+  }
+  
+  if ($submenu.length === 0) {
+    console.error("Error: submenu element not found.");
+    return;
+  }
+
+ $("#add").on("click", function() {
+    var val = $input.val();
+    if(val !== '') {
+      var elem = $("<li></li>").text(val);
+      elem.append("<button class='rem'>X</button>");
+      $mylist.append(elem);
+      $input.val("");
+    }
+  });
+
+  $mylist.on("click", ".rem", function() {
+    $(this).parent().remove();
+  });
+
+  $("#item").on("click", function() {
+    $submenu.slideToggle(500);
+  });
+});
